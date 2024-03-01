e44ff1be7c6cbfd2430bc339124b00e554093abb
Usani001 <emmanuel.usani@decagon.dev>
2024-03-01 10:03:09 +0100

    Initial commit

---
+++ src/main/java/org/pcvapp/PcvAppApplication.java
@@ -0,0 +1,13 @@
+package org.pcvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pcvapp/PcvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
